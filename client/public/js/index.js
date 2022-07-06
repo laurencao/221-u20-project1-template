78d2586f0899ceebd48cbf7cdeeae87415487ba4
Lauren Cao <lc1585@students.necsd.net>
2022-07-06 11:44:32 -0400

    this is me saving my work on github

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,11 +1,8 @@
-
-
-
-/*
- * Check ascii code for spacebar and call goToLocation in global to change URL
- */
 function checkKeyPress(e) {
   if (e.keyCode == 32) {
     goToLocation('/feed');
   }
 }
+window.addEventListener('keyup', checkKeyPress);
+document.getElementById("fade_text").addEventListener('mouseup',()=> goToLocation("/feed"));
+ 
